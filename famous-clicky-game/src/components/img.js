31c9060b40d084387533c425beb968b3e65fcc06
AbrahamTesla <manlangitv@gmail.com>
2018-09-06 22:23:10 -0400

    modify

---
+++ famous-clicky-game/src/components/img.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Images = (props)=> {
+
+       
+        return (
+            <div className = "images">
+                <div onClick={() => props.clicked(props.id)} className="img-container" >         
+                    <img alt={props.name} src={props.image }/>
+                </div>
+            </div>
+        )
+    };
+    
+
+export default Images;  
