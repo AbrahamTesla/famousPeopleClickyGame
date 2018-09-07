4d229c00dd2435236564676776e4df19e87cc5a6
AbrahamTesla <manlangitv@gmail.com>
2018-09-07 04:01:06 -0400

    modify json

--- famous-clicky-game/src/components/img.js
+++ famous-clicky-game/src/components/img.js
@@ -7,7 +7,7 @@ const Images = (props)=> {
         return (
             <div className = "images">
                 <div onClick={() => props.clicked(props.id)} className="img-container" >         
-                    <img alt={props.name} src={props.image }/>
+                    <img alt={props.name} src={props.image}/>
                 </div>
             </div>
         )
